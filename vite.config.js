464285c29c14666ffda3b3da415c54f39d89f666
Arian Akbarian <arian.akbarian83@gmail.com>
2025-10-05 22:12:44 +0330

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  // Add this base property for correct asset paths
+  base: '/React-Essentials-Project/', 
+})
